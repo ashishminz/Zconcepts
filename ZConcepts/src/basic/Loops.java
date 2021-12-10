465f4f3d93d56981795983e59062c8f07950ab62
SUPERNOVA <SUPERNOVA@DESKTOP-IPPV6C6>
2021-12-10 10:32:40 +0530

    AFter first sprint to main workspace

--- ZConcepts/src/basic/Loops.java
+++ ZConcepts/src/basic/Loops.java
@@ -5,7 +5,7 @@ public class Loops {
 	public static void main(String[] args) {
 		
 		System.out.println("Loopsghhhhhhhhhkkkkkkkkkkkkkkkkkhh");
-
+        System.out.println("Modified after 1st sprint");
 	}
 
 }
